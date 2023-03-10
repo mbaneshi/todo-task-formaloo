6abf8f48047dfbb6ad9bc5dd6f8b7f0a97bb27b2
baneshispace <baneshispace@gmail.com>
2023-03-10 15:54:00 +0330

    add dot.env

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 // src/components/App.js
-
 import { Routes, Route } from 'react-router-dom'
-
+// import supabaseClient from './supabaseClient'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import About from "./components/About";
 import Home from "./components/Home";
-
 function App() {
-  return (
 
+  console.log(process.env.REACT_APP_SUPABASE_URL)
+  return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route exact path="/dashborad" component={Dashboard} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/login" component={Login} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   )
 }
