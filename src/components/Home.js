ffa722ea37effd350a4f4cf797710b289b7bad13
baneshispace <baneshispace@gmail.com>
2023-03-10 22:55:37 +0330

    reading data from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,34 @@
 // Home.js file
+import { useEffect, useState } from "react";
+import supabaseClient from "../supabaseClient";
+
 const Home = () => {
-  return <div className="element">This is Home page</div>;
+
+  const [group, setGroup] = useState(null)
+
+  useEffect(() => {
+    supabaseClient
+      .from("group")
+      .select("*")
+      .then(({ data, error }) => {
+        if (!error) {
+          // setGroup(data)
+          console.log(data)
+          setGroup(data)
+        }
+      })
+
+  }, [])
+
+  return (
+    <>
+      <div className="element">This is group title</div>
+      {group?.map((e) => {
+        return (
+          <h1 key={e.id}>{e.title}</h1>
+        );
+      })}
+    </>
+  )
 };
 export default Home;
